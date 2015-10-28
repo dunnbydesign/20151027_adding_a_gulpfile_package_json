73f992ca1e1b3db1b7ed1124603e83af0e1f9007
Jeff Dunn <jdunn522@gmail.com>
2015-10-27 21:10:03 -0700

    Make final changes in preparation for submitting assignment

--- greet.js
+++ greet.js
@@ -2,8 +2,6 @@ module.exports.greet = function (name) {
   return 'hello ' + name
 }
 
-// For an extra point, create a command line utility that will be run using node greet.js 'some name' and will pass the input contained in that argument to the greet function and output the result to the screen.
-
 function reportInput () {
   var input = process.argv[2]
   if (input === undefined) {
@@ -12,7 +10,5 @@ function reportInput () {
     return 'Input: ' + input
   }
 }
-
-// console.log(reportInput())
 
 module.exports.reportInput = reportInput
